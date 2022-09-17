01424ba84169961c7b0f888cb9cea7d02a979d41
Rjeholge <111187701+Rjeholge@users.noreply.github.com>
2022-09-17 05:50:12 +0000

    [Update]いいね機能の追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
 root to: "homes#top"
+get "/homes/about" => "homes#about", as:"about"
 devise_for :users
 
-resources :post_images, only: [:new, :create, :index, :show]
+resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+ resource :favorites, only: [:create, :destroy]
+ resources :post_comments, only: [:create, :destroy]
+end
 
-
-get "/homes/about" => "homes#about", as:"about"
+resources :users, only: [:show, :edit, :update]
 end
